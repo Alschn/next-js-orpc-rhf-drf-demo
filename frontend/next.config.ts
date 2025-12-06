f402ab0f333498da420001a255100285fba932bc
Alschn <67019937+Alschn@users.noreply.github.com>
2025-12-06 14:38:43 +0100

    Bump all outdated packages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -13,9 +13,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 const config: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
